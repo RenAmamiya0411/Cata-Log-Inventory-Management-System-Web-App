7199b88780c16bfa2b9b25a1253671532b47a57e
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-22 04:40:08 +0800

    Added styling to Navbar.jsx

--- src/parts/Navbar.jsx
+++ src/parts/Navbar.jsx
@@ -3,17 +3,23 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav>
-      <h1>Cata-Log</h1>
-      <ul>
+    <nav className="bg-gray-800 text-white px-6 py-4 flex items-center justify-between">
+      <h1 className="text-xl font-bold">Cata-Log</h1>
+      <ul className="flex gap-6">
         <li>
-          <Link to="/">Dashboard</Link>
+          <Link to="/" className="hover:text-gray-300">
+            Dashboard
+          </Link>
         </li>
         <li>
-          <Link to="/products">Products</Link>
+          <Link to="/products" className="hover:text-gray-300">
+            Products
+          </Link>
         </li>
         <li>
-          <Link to="/categories">Categories</Link>
+          <Link to="/categories" className="hover:text-gray-300">
+            Categories
+          </Link>
         </li>
       </ul>
     </nav>
