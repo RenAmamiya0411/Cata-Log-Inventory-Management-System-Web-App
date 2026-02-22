93e70645c6222d96099f25019953940bdaa1a4a2
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-23 01:05:39 +0800

    Added product and category schemas;Server.js setup

---
+++ backend/models/Categories.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const categorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true
+    }
+  },
+  { timestamps: true }
+);
+
+module.exports("Categories", categorySchema);
