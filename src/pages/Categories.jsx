9a892f0fab4a7b59c8742eda4ebd74470a2c0064
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-22 23:24:29 +0800

    Added an AddCategoryForm in Categories.jsx

--- src/pages/Categories.jsx
+++ src/pages/Categories.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function Categories() {
+  const [newCategory, setNewCategory] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    setNewCategory("");
+  };
+
   return (
-    <div>Categories</div>
-  )
+    <div>
+      <h2>Categories</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Enter the category name"
+          value={newCategory}
+          onChange={e => setNewCategory(e.target.value)}
+          required
+        />
+        <button type="submit">Add Category</button>
+      </form>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
