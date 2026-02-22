0c2d20c26707ffd2c9c1524e76b882a7e423fbf4
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-23 05:03:02 +0800

    Set up categoryController

---
+++ backend/controllers/categoryController.js
@@ -0,0 +1,32 @@
+const Category = require("../models/Category");
+
+const getCategories = async (req, res) => {
+  try {
+    const categories = await Category.find();
+    res.json(categories);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+const addCategory = async (req, res) => {
+  try {
+    const category = new Category(req.body);
+    const savedCategory = await category.save();
+    res.status(201).json(savedCategory);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+const deleteCategory = async (req, res) => {
+  try {
+    const category = await Category.findByIdAndDelete(req.params.id);
+    if (!category) return res.status(404).json({ message: "Category not found." });
+    res.json({ message: "Category deleted." });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+module.exports = { getCategories, addCategory, deleteCategory };
