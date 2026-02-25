bc4016a03bb7df5cb30c567260ad5fcbde1b044c
Steven Ray Harvey Falcon <falconsteven25@gmail.com>
2026-02-25 21:34:24 +0800

    Updated baseURL from http to https

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://cata-log-api.onrender.com/api"
+  baseURL: "https://cata-log-api.onrender.com/api"
 });
 
 export default instance;
